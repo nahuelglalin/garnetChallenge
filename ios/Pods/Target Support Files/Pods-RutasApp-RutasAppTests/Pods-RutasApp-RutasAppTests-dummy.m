5872bfab0cf22d57d56392211b62d3e5cbf4e444
nahuelglalin <nahuelglalin@gmail.com>
2022-06-12 00:53:41 -0300

    Fixes

---
+++ ios/Pods/Target Support Files/Pods-RutasApp-RutasAppTests/Pods-RutasApp-RutasAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RutasApp_RutasAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_RutasApp_RutasAppTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-RutasApp/Pods-RutasApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RutasApp : NSObject
+@end
+@implementation PodsDummy_Pods_RutasApp
+@end
